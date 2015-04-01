c6db991150a485fe5a6ab208ec75c54939cf542c
Odint <aymanekhouaji@live.com>
2015-04-01 11:24:45 +0200

    Ajout de la balise <script > pour jQuery

--- includes/header.php
+++ includes/header.php
@@ -5,5 +5,6 @@
     
         <meta charset="utf-8">
         <link rel="stylesheet" href="styles/style.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 </head>
 <body>
